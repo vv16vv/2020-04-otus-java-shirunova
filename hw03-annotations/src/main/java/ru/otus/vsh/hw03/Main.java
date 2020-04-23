7a35b715e00a6c7a766b421e34a82b5c34459dee
Viktoria Shirunova <29532514+vv16vv@users.noreply.github.com>
2020-04-23 13:00:09 +0300

    Changes according to PR comments
    Согласна с Вашим замечанием и Runner стал обычным классом. Вложенный класс переименован в TestInfo, что лучше отражает цель класса - хранение вспомогательной информации для каждого тестового метода. Так как тестовых методов может быть несколько, то если выносить эти параметры в класс Runner - пришлось бы делать какие-то дополнительные массивы или мапы по одному на каждый параметр, что на мой взгляд не очень красиво. Общие данные - списки методов Before и After - вынесены, они не меняются от одного тестового метода к другому. Метод executeTest переименован в executeTestSet, чтобы подчеркнуть, что исполняется не один тестовый метод, а целый набор методов. Счетчики внутри этого метода я оставила локальными - мне кажется, что они это внутренняя реализация подсчета статистики, а далее на выход идет упорядоченная мапа (по сути мне просто нужен тут массив где индексом является статус). Мне хотелось бы тут отделить место где вычисляется статистика от мест где используются результаты.

--- hw03-annotations/src/main/java/ru/otus/vsh/hw03/Main.java
+++ hw03-annotations/src/main/java/ru/otus/vsh/hw03/Main.java
@@ -1,6 +1,7 @@
 package ru.otus.vsh.hw03;
 
 import ru.otus.vsh.hw03.runner.Runner;
+import ru.otus.vsh.hw03.runner.TestStatus;
 
 import java.util.Map;
 
@@ -10,18 +11,22 @@ public class Main {
             if (args.length == 0)
                 throw new IllegalArgumentException("Not enough parameters. Specify a fully qualified name of the tested class." +
                         "\nUse one of following:" +
-                        "\n\tru.otus.vsh.hw03.tests.ComplexTest" +
                         "\n\tru.otus.vsh.hw03.tests.EmptyTest" +
                         "\n\tru.otus.vsh.hw03.tests.EmptyWithBeforeAfterTest" +
                         "\n\tru.otus.vsh.hw03.tests.ExceptionInAfterTest" +
                         "\n\tru.otus.vsh.hw03.tests.ExceptionInBeforeTest" +
                         "\n\tru.otus.vsh.hw03.tests.ExceptionInTestTest" +
-                        "\n\tru.otus.vsh.hw03.tests.SimpleTest");
-            Map<Runner.Status, Integer> tests = Runner.run(args[0]);
+                        "\n\tru.otus.vsh.hw03.tests.SimpleTest" +
+                        "\n\tru.otus.vsh.hw03.tests.SeveralTestsTest" +
+                        "\n\tru.otus.vsh.hw03.tests.WithTimesTest");
 
-            System.out.println("Status: passed = " + tests.get(Runner.Status.SUCCESS) +
-                    "; failed = " + tests.get(Runner.Status.FAILED) +
-                    "; total = " + (tests.get(Runner.Status.SUCCESS) + tests.get(Runner.Status.FAILED)));
+            Runner runner = new Runner(args[0]);
+            runner.run();
+            Map<TestStatus, Integer> tests = runner.getResult();
+
+            System.out.println("Status: passed = " + tests.get(TestStatus.SUCCESS) +
+                    "; failed = " + tests.get(TestStatus.FAILED) +
+                    "; total = " + tests.size());
         } catch (Exception e) {
             e.printStackTrace();
         }
--- hw03-annotations/src/main/java/ru/otus/vsh/hw03/runner/Runner.java
+++ hw03-annotations/src/main/java/ru/otus/vsh/hw03/runner/Runner.java
@@ -1,5 +1,6 @@
 package ru.otus.vsh.hw03.runner;
 
+import com.google.common.collect.Maps;
 import ru.otus.vsh.hw03.annotations.After;
 import ru.otus.vsh.hw03.annotations.Before;
 import ru.otus.vsh.hw03.annotations.Test;
@@ -11,14 +12,66 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+/**
+ * один экземпляр отвечает за запуск одного тестового класса.
+ */
 public class Runner {
-    public enum Status {SUCCESS, FAILED}
 
     private final Class<?> aClass;
 
-    private Runner(String className) throws ClassNotFoundException {
+    private final List<Method> beforeMethods;
+    private final List<Method> afterMethods;
+    private final List<TestInfo> testMethods;
+
+    private final Map<TestStatus, Integer> result;
+
+    /**
+     * @param className - FQN тестового класса
+     * @throws ClassNotFoundException если класс, заданный параметром, не находится
+     */
+    public Runner(String className) throws ClassNotFoundException {
         aClass = Class.forName(className);
+        beforeMethods = getAnnotatedMethods(Before.class);
+        afterMethods = getAnnotatedMethods(After.class);
+        testMethods = getAnnotatedMethods(Test.class).stream().map(TestInfo::new).collect(Collectors.toList());
+
+        result = Maps.newEnumMap(TestStatus.class);
+        initializeResults();
+    }
+
+    /**
+     * @return статистику: сколько всего было запусков (с учетов повторений от аннотации [Times])
+     * и сколько из них в каждом поддерживаемом статусе (пока только Success и Failed)
+     */
+    public Map<TestStatus, Integer> getResult() {
+        return Maps.immutableEnumMap(result);
+    }
+
+    /**
+     * Запуск одного тестового метода.
+     * Здесь есть 2 дополнительных момента:
+     * - Во-первых, один запуск тестового метода состоит из трех этапов:
+     * -- запуск в произвольном порядке всех методов, помеченных аннотацией [Before];
+     * -- запуск собственно тестового метода – метода, помеченного аннотацией [Test];
+     * -- запуск в произвольном порядке всех методов, помеченных аннотацией [After].
+     * - Во-вторых, каждый набор этапов для каждого тестового метода запускается столько раз
+     * сколько указано в аннотации [Times].
+     * Если эта аннотация не задана – то запуск происходит только 1 раз.
+     */
+    public void run() {
+        if (testMethods.isEmpty())
+            throw new IllegalStateException("Test class does not contain any test method (with annotation @Test)");
+
+        initializeResults();
+
+        for (TestInfo data : testMethods) {
+            Map<TestStatus, Integer> result = executeTestSet(data);
+            for (TestStatus status : TestStatus.values()) {
+                this.result.put(status, this.result.get(status) + result.get(status));
+            }
+        }
     }
 
     @Nonnull
@@ -33,71 +86,7 @@ public class Runner {
         return result;
     }
 
-    @Nonnull
-    private List<Method> getBeforeMethods() {
-        return getAnnotatedMethods(Before.class);
-    }
-
-    @Nonnull
-    private List<Method> getAfterMethods() {
-        return getAnnotatedMethods(After.class);
-    }
-
-    @Nonnull
-    private List<Method> getTestMethods() {
-        return getAnnotatedMethods(Test.class);
-    }
-
-    private static class TestExecutionData {
-        private final Method testMethod;
-        private final List<Method> beforeMethods;
-        private final List<Method> afterMethods;
-        private final int times;
-        private final String name;
-
-        public TestExecutionData(Method testMethod,
-                                 List<Method> beforeMethods,
-                                 List<Method> afterMethods) {
-            this.testMethod = testMethod;
-            String annotationValue = this.testMethod.getAnnotation(Test.class).value();
-            if (annotationValue.isEmpty()) {
-                this.name = this.testMethod.getName();
-            } else this.name = annotationValue;
-            this.beforeMethods = beforeMethods;
-            this.afterMethods = afterMethods;
-            if (this.testMethod.isAnnotationPresent(Times.class)) {
-                this.times = this.testMethod.getAnnotation(Times.class).value();
-            } else {
-                this.times = 1;
-            }
-        }
-    }
-
-    @Nonnull
-    public static Map<Status, Integer> run(String className) throws ClassNotFoundException {
-        Runner runner = new Runner(className);
-
-        List<Method> beforeMethods = runner.getBeforeMethods();
-        List<Method> afterMethods = runner.getAfterMethods();
-        List<Method> testMethods = runner.getTestMethods();
-
-        if (testMethods.isEmpty())
-            throw new IllegalStateException("Test class does not contain any test method (with annotation @Test)");
-
-        List<TestExecutionData> executionData = new ArrayList<>(testMethods.size());
-        testMethods.forEach((method) -> executionData.add(new TestExecutionData(method, beforeMethods, afterMethods)));
-
-        int passed = 0;
-        int failed = 0;
-        for (TestExecutionData data : executionData) {
-            Map<Status, Integer> result = runner.executeTest(data);
-            passed += result.get(Status.SUCCESS);
-            failed += result.get(Status.FAILED);
-        }
-        return Map.of(Status.SUCCESS, passed, Status.FAILED, failed);
-    }
-
-    private Map<Status, Integer> executeTest(TestExecutionData data) {
+    private Map<TestStatus, Integer> executeTestSet(TestInfo info) {
         Object testObject;
         int passed = 0;
         int failed = 0;
@@ -106,22 +95,22 @@ public class Runner {
         } catch (Exception e) {
             throw new IllegalStateException("Error on creating an object of type " + aClass.getSimpleName(), e);
         }
-        for (int i = 0; i < data.times; i++) {
-            System.out.println("Running: '" + data.name + "'. Run #" + (i + 1) + "......");
+        for (int i = 0; i < info.times; i++) {
+            System.out.println("Running: '" + info.name + "'. Run #" + (i + 1) + "......");
             boolean hasPassed = false;
             try {
-                for (Method method : data.beforeMethods) {
+                for (Method method : beforeMethods) {
                     method.setAccessible(true);
                     method.invoke(testObject);
                 }
-                data.testMethod.setAccessible(true);
-                data.testMethod.invoke(testObject);
+                info.testMethod.setAccessible(true);
+                info.testMethod.invoke(testObject);
                 hasPassed = true;
             } catch (Throwable e) {
                 e.printStackTrace();
             } finally {
                 try {
-                    for (Method method : data.afterMethods) {
+                    for (Method method : afterMethods) {
                         method.setAccessible(true);
                         method.invoke(testObject);
                     }
@@ -133,6 +122,38 @@ public class Runner {
             if (hasPassed) passed++;
             else failed++;
         }
-        return Map.of(Status.SUCCESS, passed, Status.FAILED, failed);
+        return Map.of(TestStatus.SUCCESS, passed, TestStatus.FAILED, failed);
+    }
+
+    private void initializeResults() {
+        for (TestStatus status : TestStatus.values()) {
+            result.put(status, 0);
+        }
+    }
+
+    /**
+     * Класс [TestInfo] предназначен для хранения дополнительной
+     * информации для каждого тестового метода:
+     * [testMethod] - сам тестовый метод
+     * [name] - читаемое имя – задается параметром [value] аннотации [Test]
+     * [times] - количество повторений – задается аннотацией [Times]
+     */
+    private static class TestInfo {
+        private final Method testMethod;
+        private final int times;
+        private final String name;
+
+        public TestInfo(Method testMethod) {
+            this.testMethod = testMethod;
+            String annotationValue = this.testMethod.getAnnotation(Test.class).value();
+            if (annotationValue.isEmpty()) {
+                this.name = this.testMethod.getName();
+            } else this.name = annotationValue;
+            if (this.testMethod.isAnnotationPresent(Times.class)) {
+                this.times = this.testMethod.getAnnotation(Times.class).value();
+            } else {
+                this.times = 1;
+            }
+        }
     }
 }
