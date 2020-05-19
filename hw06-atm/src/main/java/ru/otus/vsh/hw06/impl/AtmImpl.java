bb6d557538084b44c10363d25540f2dc38e0c926
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2020-05-19 13:54:39 +0300

    HW-06: added interface Cell for representing a separate cell with banknotes of one nominal and its implementation CellImpl. Class AtmImpl has been changed correspondingly.

--- hw06-atm/src/main/java/ru/otus/vsh/hw06/impl/AtmImpl.java
+++ hw06-atm/src/main/java/ru/otus/vsh/hw06/impl/AtmImpl.java
@@ -1,65 +1,73 @@
 package ru.otus.vsh.hw06.impl;
 
-import com.google.common.collect.Maps;
 import ru.otus.vsh.hw06.api.Atm;
 import ru.otus.vsh.hw06.api.Banknote;
 import ru.otus.vsh.hw06.api.CantHandOutMoneyException;
+import ru.otus.vsh.hw06.api.Cell;
 
+import javax.annotation.Nonnull;
+import java.util.Comparator;
 import java.util.EnumMap;
-import java.util.HashMap;
 import java.util.Map;
 
 public class AtmImpl implements Atm {
-    private final Map<Banknote, Integer> cells = new EnumMap<>(Banknote.class);
+    private final Map<Banknote, Cell> cells = new EnumMap<>(Banknote.class);
     private Banknote minimal = null;
 
     @Override
-    public void accept(Map<Banknote, Integer> income) {
+    public void accept(@Nonnull Map<Banknote, Integer> income) {
         income.forEach((note, quantity) -> {
-            if (quantity < 0)
-                throw new IllegalStateException(String.format("Количество банкнот не может быть отрицательным (%d)", quantity));
-            if (minimal == null || minimal.getNominal() > note.getNominal()) minimal = note;
-            Integer currentQuantity = cells.getOrDefault(note, 0);
-            cells.put(note, quantity + currentQuantity);
+            cells.putIfAbsent(note, new CellImpl(note));
+            cells.get(note).add(quantity);
         });
+        calculateMinimalBanknote();
+    }
+
+    private void calculateMinimalBanknote() {
+        minimal = cells.values().stream()
+                .filter(cell -> !cell.isEmpty())
+                .map(Cell::getBanknote)
+                .min(Comparator.comparingLong(Banknote::getNominal))
+                .orElseThrow(IllegalStateException::new);
     }
 
     @Override
     public Map<Banknote, Integer> handout(long sum) {
-        if (sum <= 0 )
+        if (minimal == null)
+            throw new IllegalStateException("Банкомат не загружен");
+        if (sum <= 0) {
             throw new IllegalArgumentException(String.format("Не могу выдать отрицательную сумму %s", sum));
-        if (minimal == null )
-            throw new IllegalStateException("Банкомат не инициализирован");
+        }
         if (sum % minimal.getNominal() != 0 || sum > currentValue())
             throw new CantHandOutMoneyException(sum);
-        Map<Banknote, Integer> result = new HashMap<>();
-        var copyCells = Maps.newEnumMap(cells);
+        Map<Banknote, Integer> result = new EnumMap<>(Banknote.class);
         long remainingSum = sum;
-        var iterator = copyCells.keySet().iterator();
+        var iterator = cells.values().iterator();
         while (remainingSum > 0 && iterator.hasNext()) {
-            Banknote note = iterator.next();
-            int expectedQuantity = (int) (remainingSum / note.getNominal());
-            int presentQuantity = copyCells.getOrDefault(note, 0);
+            Cell cell = iterator.next();
+            int presentQuantity = cell.getCount();
+            if (presentQuantity == 0) continue;
+            int expectedQuantity = (int) (remainingSum / cell.getBanknote().getNominal());
             if (expectedQuantity > presentQuantity) expectedQuantity = presentQuantity;
             if (expectedQuantity > 0) {
-                result.put(note, expectedQuantity);
-                remainingSum = remainingSum - expectedQuantity * note.getNominal();
-                copyCells.put(note, presentQuantity - expectedQuantity);
+                result.put(cell.getBanknote(), expectedQuantity);
+                remainingSum = remainingSum - expectedQuantity * cell.getBanknote().getNominal();
             }
         }
         if (remainingSum > 0) {
             throw new CantHandOutMoneyException(sum);
-        }
-        else {
-            cells.putAll(copyCells);
+        } else {
+            result.forEach((key, value) -> cells.get(key).handout(value));
+            calculateMinimalBanknote();
         }
         return result;
     }
 
     @Override
     public long currentValue() {
-        return cells.keySet().stream()
-                .mapToLong(note -> note.getNominal() * cells.getOrDefault(note, 0))
-                .sum();
+        return cells.values().stream()
+                .filter(cell -> !cell.isEmpty())
+                .map(Cell::getSum)
+                .reduce(0L, Long::sum);
     }
 }
