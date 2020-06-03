de24cbe101a631f366ab09535565257b0598f04c
Viktoria Shirunova <29532514+vv16vv@users.noreply.github.com>
2020-06-03 19:44:31 +0300

    hw07-atm02: added several tests to DepartmentTest

--- hw07-atm2/src/main/java/ru/otus/vsh/hw07/Main.java
+++ hw07-atm2/src/main/java/ru/otus/vsh/hw07/Main.java
@@ -1,6 +1,9 @@
 package ru.otus.vsh.hw07;
 
-import ru.otus.vsh.hw07.api.*;
+import ru.otus.vsh.hw07.api.Atm;
+import ru.otus.vsh.hw07.api.AtmBuilder;
+import ru.otus.vsh.hw07.api.Banknote;
+import ru.otus.vsh.hw07.api.CantHandOutMoneyException;
 import ru.otus.vsh.hw07.api.actions.AtmAction;
 import ru.otus.vsh.hw07.api.actions.AtmGetValueAction;
 import ru.otus.vsh.hw07.api.actions.AtmInitiateAction;
@@ -14,11 +17,10 @@ import java.util.Date;
 import java.util.Map;
 
 public class Main {
-    private final String datePattern = "dd MMM yyyy";
-    private final DateFormat dateFormat = new SimpleDateFormat(datePattern);
+    private final static String datePattern = "dd MMM yyyy";
+    private final static DateFormat dateFormat = new SimpleDateFormat(datePattern);
 
     public static void main(String[] args) {
-        var main = new Main();
         Department department = new Department("Приморский");
         Atm emptyAtm = new AtmBuilder("first").build();
         emptyAtm.accept(Map.of(
@@ -31,24 +33,25 @@ public class Main {
                 .build();
         department.addAtm(emptyAtm);
         department.addAtm(atmWithMoney);
+        (new AtmGetValueAction()).process(department);
 
         AtmAction putSomeMoney = new AtmPutMoneyAction(Map.of(
                 Banknote.FIVE_HUNDRED, 100
         )).next(new AtmGetValueAction());
-        AtmAction dayOpener = new AtmInitiateAction(String.format("Открыть день %s", main.dateFormat.format(new Date()))).next(putSomeMoney);
+        AtmAction dayOpener = new AtmInitiateAction(String.format("Открыть день %s", dateFormat.format(new Date()))).next(putSomeMoney);
         dayOpener.process(department);
 
-        main.processOutput(emptyAtm, 1000);
-        main.printCurrentMoneyState(department);
-        main.processOutput(atmWithMoney, 5000);
-        main.printCurrentMoneyState(department);
+        processOutput(emptyAtm, 1000);
+        printCurrentMoneyState(department);
+        processOutput(atmWithMoney, 5000);
+        printCurrentMoneyState(department);
 
-        AtmAction dayCloser = new AtmGetValueAction().next(new AtmInitiateAction(String.format("Закрыть день %s", main.dateFormat.format(new Date()))));
+        AtmAction dayCloser = new AtmGetValueAction().next(new AtmInitiateAction(String.format("Закрыть день %s", dateFormat.format(new Date()))));
         dayCloser.process(department);
 
     }
 
-    private void processOutput(@Nonnull Atm atm, long sum) {
+    private static void processOutput(@Nonnull Atm atm, long sum) {
         try {
             var result = atm.handout(sum);
             System.out.printf("ATM '%s' выдал %d рублей следующими купюрами: %s%n", atm.id(), sum, result.toString());
@@ -57,7 +60,7 @@ public class Main {
         }
     }
 
-    private void printCurrentMoneyState(@Nonnull Department department){
+    private static void printCurrentMoneyState(@Nonnull Department department) {
         System.out.printf("Отделение '%s': текущий остаток (без опроса всех банкоматов) %d рублей%n", department.id(), department.getCurrentMoney());
     }
 
