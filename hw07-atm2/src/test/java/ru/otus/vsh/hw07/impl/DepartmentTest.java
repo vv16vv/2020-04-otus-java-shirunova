563cef4b0714ccc9b621c1744ef43dbb614ef8e3
Viktoria Shirunova <29532514+vv16vv@users.noreply.github.com>
2020-06-03 19:44:31 +0300

    hw07-atm02: added several tests to DepartmentTest

--- hw07-atm2/src/test/java/ru/otus/vsh/hw07/impl/DepartmentTest.java
+++ hw07-atm2/src/test/java/ru/otus/vsh/hw07/impl/DepartmentTest.java
@@ -5,6 +5,8 @@ import ru.otus.vsh.hw07.api.AtmBuilder;
 import ru.otus.vsh.hw07.api.Banknote;
 import ru.otus.vsh.hw07.api.actions.AtmAction;
 import ru.otus.vsh.hw07.api.actions.AtmGetValueAction;
+import ru.otus.vsh.hw07.api.actions.AtmInitiateAction;
+import ru.otus.vsh.hw07.api.actions.AtmPutMoneyAction;
 
 import java.util.Map;
 
@@ -73,5 +75,34 @@ public class DepartmentTest {
                 Banknote.FIVE_HUNDRED, 1
         ));
         assert department.getCurrentMoney() == 560L;
+    }
+
+    @Test
+    void acceptMoneyAction() {
+        var department = new Department("store");
+        var atm1 = new AtmBuilder("first").build();
+        var atm2 = new AtmBuilder("second").build();
+        department.addAtm(atm1);
+        department.addAtm(atm2);
+        var putMoneyAction = new AtmPutMoneyAction(Map.of(
+                Banknote.FIVE_HUNDRED, 2
+        )).next(new AtmGetValueAction());
+        putMoneyAction.process(department);
+        assert department.getCurrentMoney() == 1000L;
+    }
+
+    @Test
+    void acceptMoneyResetAction() {
+        var department = new Department("store");
+        var atm1 = new AtmBuilder("first").build();
+        var atm2 = new AtmBuilder("second").build();
+        department.addAtm(atm1);
+        department.addAtm(atm2);
+        var putMoneyAction = new AtmPutMoneyAction(Map.of(
+                Banknote.FIVE_HUNDRED, 2
+        )).next(new AtmGetValueAction());
+        AtmAction initiateAction = new AtmInitiateAction("Сбросить к исходному состоянию").next(putMoneyAction);
+        initiateAction.process(department);
+        assert department.getCurrentMoney() == 1000L;
     }
 }
