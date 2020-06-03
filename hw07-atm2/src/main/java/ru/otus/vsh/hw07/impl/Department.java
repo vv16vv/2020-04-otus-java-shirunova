9429791730117bafdae3eb876756bb4fa6ff9d35
Viktoria Shirunova <29532514+vv16vv@users.noreply.github.com>
2020-06-03 15:08:41 +0300

    hw07-atm02: use AtomicLong instead of just long

--- hw07-atm2/src/main/java/ru/otus/vsh/hw07/impl/Department.java
+++ hw07-atm2/src/main/java/ru/otus/vsh/hw07/impl/Department.java
@@ -1,6 +1,8 @@
 package ru.otus.vsh.hw07.impl;
 
-import ru.otus.vsh.hw07.api.*;
+import ru.otus.vsh.hw07.api.Atm;
+import ru.otus.vsh.hw07.api.Banknote;
+import ru.otus.vsh.hw07.api.ValueChangeOperation;
 import ru.otus.vsh.hw07.api.listeners.AtmInitiateListener;
 import ru.otus.vsh.hw07.api.listeners.AtmValueChangeListener;
 
@@ -8,11 +10,12 @@ import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Department implements AtmValueChangeListener, AtmInitiateListener {
     private final List<Atm> atms = new ArrayList<>();
     private final String id;
-    private volatile long currentMoney = 0;
+    private final AtomicLong currentMoney = new AtomicLong(0L);
 
     public Department(String id) {
         this.id = id;
@@ -49,7 +52,7 @@ public class Department implements AtmValueChangeListener, AtmInitiateListener {
     }
 
     public long getCurrentMoney() {
-        return currentMoney;
+        return currentMoney.get();
     }
 
     public String id() {
@@ -58,10 +61,8 @@ public class Department implements AtmValueChangeListener, AtmInitiateListener {
 
     @Override
     public void onValueChange(long delta, ValueChangeOperation operation) {
-        synchronized (this) {
-            if (operation == ValueChangeOperation.HAND_OUT) currentMoney -= delta;
-            else currentMoney += delta;
-        }
+        if (operation == ValueChangeOperation.HAND_OUT) currentMoney.getAndAdd(-1 * delta) ;
+        else currentMoney.getAndAdd(delta);
     }
 
     @Override
