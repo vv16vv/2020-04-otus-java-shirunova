b6060edfa7e994e3644154548602271ebd02e104
Viktoria Shirunova <29532514+vv16vv@users.noreply.github.com>
2020-06-03 19:02:16 +0300

    hw07-atm02: added several tests for Department

--- hw07-atm2/src/test/java/ru/otus/vsh/hw07/impl/DepartmentTest.java
+++ hw07-atm2/src/test/java/ru/otus/vsh/hw07/impl/DepartmentTest.java
@@ -3,6 +3,8 @@ package ru.otus.vsh.hw07.impl;
 import org.junit.jupiter.api.Test;
 import ru.otus.vsh.hw07.api.AtmBuilder;
 import ru.otus.vsh.hw07.api.Banknote;
+import ru.otus.vsh.hw07.api.actions.AtmAction;
+import ru.otus.vsh.hw07.api.actions.AtmGetValueAction;
 
 import java.util.Map;
 
@@ -32,6 +34,44 @@ public class DepartmentTest {
                 .setInitialMoney(initialState)
                 .build();
         department.addAtm(atm);
+        AtmAction getValueAction = new AtmGetValueAction();
+        getValueAction.process(department);
         assert department.getCurrentMoney() == 60L;
+    }
+
+    @Test
+    void handOutMoney() {
+        var department = new Department("store");
+        var initialState = Map.of(
+                Banknote.FIFTY, 1,
+                Banknote.TEN, 1
+        );
+        var atm = new AtmBuilder("first")
+                .setInitialMoney(initialState)
+                .build();
+        department.addAtm(atm);
+        AtmAction getValueAction = new AtmGetValueAction();
+        getValueAction.process(department);
+        atm.handout(50L);
+        assert department.getCurrentMoney() == 10L;
+    }
+
+    @Test
+    void acceptMoney() {
+        var department = new Department("store");
+        var initialState = Map.of(
+                Banknote.FIFTY, 1,
+                Banknote.TEN, 1
+        );
+        var atm = new AtmBuilder("first")
+                .setInitialMoney(initialState)
+                .build();
+        department.addAtm(atm);
+        AtmAction getValueAction = new AtmGetValueAction();
+        getValueAction.process(department);
+        atm.accept(Map.of(
+                Banknote.FIVE_HUNDRED, 1
+        ));
+        assert department.getCurrentMoney() == 560L;
     }
 }
