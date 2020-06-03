e9aadd329dbff5acb32aa1427f18058bd0fea5a0
Viktoria Shirunova <29532514+vv16vv@users.noreply.github.com>
2020-06-03 14:44:04 +0300

    hw07-atm02: listeners moved to "listeners" package

--- hw07-atm2/src/main/java/ru/otus/vsh/hw07/api/listeners/AtmValueChangeListener.java
+++ hw07-atm2/src/main/java/ru/otus/vsh/hw07/api/listeners/AtmValueChangeListener.java
@@ -1,4 +1,6 @@
-package ru.otus.vsh.hw07.api;
+package ru.otus.vsh.hw07.api.listeners;
+
+import ru.otus.vsh.hw07.api.ValueChangeOperation;
 
 public interface AtmValueChangeListener {
     void onValueChange(long delta, ValueChangeOperation operation);
