4bb0cbfdddc2702323a2a19c18409f7d52639578
Viktoria Shirunova <29532514+vv16vv@users.noreply.github.com>
2020-06-12 18:08:07 +0300

    [hw09-orm] supported different types

---
+++ hw09-orm/src/main/java/ru/otus/jdbc/mapper/impl/types/IntType.java
@@ -0,0 +1,14 @@
+package ru.otus.jdbc.mapper.impl.types;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class IntType implements DbColumnType {
+    @Override
+    @Nullable
+    public Object getValue(@Nonnull ResultSet rs, @Nonnull String columnName) throws SQLException {
+        return rs.getInt(columnName);
+    }
+}
---
+++ hw09-orm/src/main/java/ru/otus/jdbc/mapper/impl/types/StringType.java
@@ -0,0 +1,14 @@
+package ru.otus.jdbc.mapper.impl.types;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class StringType implements DbColumnType {
+    @Override
+    @Nullable
+    public Object getValue(@Nonnull ResultSet rs, @Nonnull String columnName) throws SQLException {
+        return rs.getString(columnName);
+    }
+}
