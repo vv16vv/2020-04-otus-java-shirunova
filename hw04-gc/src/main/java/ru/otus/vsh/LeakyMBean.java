706c61b316c2fd90b28416f2b2272748391c8e19
Viktoria Shirunova <29532514+vv16vv@users.noreply.github.com>
2020-06-23 22:06:50 +0300

    [hw04-gc] added investigation code

---
+++ hw04-gc/src/main/java/ru/otus/vsh/LeakyMBean.java
@@ -0,0 +1,11 @@
+package ru.otus.vsh;
+
+public interface LeakyMBean {
+    int getQuantAdd();
+
+    void setQuantAdd(int quantAdd);
+
+    int getQuantRemove();
+
+    void setQuantRemove(int quantRemove);
+}
