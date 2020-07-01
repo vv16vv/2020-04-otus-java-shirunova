0fdb1ba6d6c942fd98abc33351bd7a5b2509fba9
Viktoria Shirunova <29532514+vv16vv@users.noreply.github.com>
2020-07-01 19:21:15 +0300

    [hw12-webserver] correct UserDaoHibernate

--- hw12-webserver/src/main/java/ru/otus/vsh/hw12/AdministrationWebServerDemo.java
+++ hw12-webserver/src/main/java/ru/otus/vsh/hw12/AdministrationWebServerDemo.java
@@ -54,31 +54,31 @@ public class AdministrationWebServerDemo {
 
     private static void initiateData(UserDao userDao, RoleDao roleDao){
         DBService<Role> dbServiceRole = new DbServiceRoleImpl(roleDao);
-        var role1 = new Role(1,"user");
-        var role2 = new Role(2,"admin");
-        var role3 = new Role(3,"guest");
+        var roleUser = new Role(0,"user");
+        var roleAdmin = new Role(0,"admin");
+        var roleGuest = new Role(0,"guest");
 
-        dbServiceRole.saveObject(role1);
-        dbServiceRole.saveObject(role2);
-        dbServiceRole.saveObject(role3);
+        dbServiceRole.saveObject(roleUser);
+        dbServiceRole.saveObject(roleAdmin);
+        dbServiceRole.saveObject(roleGuest);
 
         DBService<User> dbServiceUser = new DbServiceUserImpl(userDao);
 
-        var address1 = new Address(1, "Планерная улица");
-        var address2 = new Address(2, "Проспект Авиаконструкторов");
-        var address3 = new Address(3, "Комендантский проспект");
+        var address1 = new Address(0, "Планерная улица");
+        var address2 = new Address(0, "Проспект Авиаконструкторов");
+        var address3 = new Address(0, "Комендантский проспект");
 
-        var phone1 = new Phone(1, "+7(123)456-78-90");
-        var phone2 = new Phone(2, "+7(987)876-23-23");
-        var phone3 = new Phone(3, "+7(111)222-33-44");
-        var phone4 = new Phone(4, "+7(098)124-63-74");
+        var phone1 = new Phone(0, "+7(123)456-78-90");
+        var phone2 = new Phone(0, "+7(987)876-23-23");
+        var phone3 = new Phone(0, "+7(111)222-33-44");
+        var phone4 = new Phone(0, "+7(098)124-63-74");
 
-        var user1 = new User(1, "vitkus", "Виктория", "12345", address1, role2, new ArrayList<>());
+        var user1 = new User(0, "vitkus", "Виктория", "12345", address1, roleAdmin, new ArrayList<>());
         user1.addPhone(phone1);
         user1.addPhone(phone2);
-        var user2 = new User(2, "sevantius", "Всеволод", "32321", address2, role3, new ArrayList<>());
+        var user2 = new User(0, "sevantius", "Всеволод", "11111", address2, roleGuest, new ArrayList<>());
         user2.addPhone(phone3);
-        var user3 = new User(3, "koshir", "Константин", "91929", address3, role1, new ArrayList<>());
+        var user3 = new User(0, "koshir", "Константин", "24680", address3, roleUser, new ArrayList<>());
         user3.addPhone(phone4);
 
         dbServiceUser.saveObject(user1);
