cfa0956a02540817d3f55931f7c246c08972616c
Viktoria Shirunova <29532514+vv16vv@users.noreply.github.com>
2020-07-01 17:59:01 +0300

    [hw12-webserver] missed files

--- hw12-webserver/src/main/java/ru/otus/dbCore/dbService/DbServiceUserImpl.java
+++ hw12-webserver/src/main/java/ru/otus/dbCore/dbService/DbServiceUserImpl.java
@@ -4,11 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.otus.dbCore.dao.UserDao;
 import ru.otus.dbCore.dbService.api.AbstractDbServiceImpl;
-import ru.otus.dbCore.dbService.api.DbServiceException;
 import ru.otus.dbCore.model.User;
-import ru.otus.dbCore.sessionmanager.SessionManager;
-
-import java.util.Optional;
 
 public class DbServiceUserImpl extends AbstractDbServiceImpl<User> {
     private static final Logger logger = LoggerFactory.getLogger(DbServiceUserImpl.class);
